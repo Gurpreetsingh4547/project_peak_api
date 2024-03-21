aaaf355306b430e786c61f8c8ddc4eb728ad24d0
Gurpreetsingh4547 <gu07869prince@gmail.com>
2024-03-21 21:31:28 +0530

    Configure CORS origin.

--- app.js
+++ app.js
@@ -1,26 +1,50 @@
+// Packages
 import express from "express";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import cors from "cors";
+import { config } from "dotenv";
+
+// Configure env
+config({ path: "./config/config.env" });
 
 // Routers
 import Routers from "./Routers/index.js";
 
+// Configure expree app
 export const app = express();
 
+// Middleware
 app.use(express.json());
+
+// Parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
+
+// Parse cookies
 app.use(cookieParser());
+
+// File Upload
 app.use(
   fileUpload({
     useTempFiles: true,
     limits: { fileSize: 50 * 1024 * 1024 },
   })
 );
-app.use(cors());
 
+// Cors Options
+const corsOptions = {
+  origin: process.env.APP_URL.split(" "),
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+// Cors
+app.use(cors(corsOptions));
+
+// Routes
 app.use("/api/v1", Routers);
 
+// Home route or Defaut router
 app.get("/", (req, res) => {
   res.send("Server is working!");
 });
--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 import { app } from "./app.js";
 
-// Packages
-import { config } from "dotenv";
-
 // Services
 import { connectDatabase } from "./config/database.js";
-
-// Configure env
-config({ path: "./config/config.env" });
 
 // Connect Database
 connectDatabase();
 
+// Start server
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
