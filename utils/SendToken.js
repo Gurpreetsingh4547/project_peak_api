7ffa09fb43f34244d53d91b3cf4e1aebde685097
Gurpreetsingh4547 <gu07869prince@gmail.com>
2024-04-09 22:12:29 +0530

    Change the set cookie options.

--- utils/SendToken.js
+++ utils/SendToken.js
@@ -6,7 +6,7 @@ export const SendToken = (res, user, statusCode, message) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
-    sameSite: "none",
+    sameSite: "strict",
     secure: true,
   };
 
